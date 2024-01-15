a1203a3c527ccbf7337da48ad24768bdf426ffb8
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2024-01-15 15:21:49 +0200

    fix: Failing tests

--- src/Testing/MakesVersionedHttpRequests.php
+++ src/Testing/MakesVersionedHttpRequests.php
@@ -5,6 +5,7 @@ namespace TenantCloud\APIVersioning\Testing;
 use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests as LaravelMakesHttpRequests;
 use TenantCloud\APIVersioning\Version\RequestHeaderVersionParser;
 use Tests\MakesVersionedHttpRequestsTest;
+use Webmozart\Assert\Assert;
 
 /**
  * @see MakesVersionedHttpRequestsTest
@@ -18,6 +19,8 @@ trait MakesVersionedHttpRequests
 	public function withApiVersion(string $apiVersion): self
 	{
 		$this->withHeader(RequestHeaderVersionParser::HEADER, $apiVersion);
+
+		Assert::notNull($this->app);
 
 		$this->app->terminating(function () {
 			unset($this->defaultHeaders[RequestHeaderVersionParser::HEADER]);
